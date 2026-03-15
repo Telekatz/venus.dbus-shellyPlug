1fde4523bef270f3dfb2bfe75993e23faf38b46b
Telekatz <telekatz@gmx.de>
2026-03-15 17:19:17 +0100

    GUI V2 installer fixed

--- installGuiV2.sh
+++ installGuiV2.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 ZIP_URL="https://github.com/Telekatz/gui-v2/releases/latest/download/venus-webassembly.zip"
-TARGET_DIR="/var/www/venus/gui-v2-mod"
-SYMLINK="/var/www/venus/gui-v2"
-BACKUP_SYMLINK="/var/www/venus/gui-v2-backup"
+TARGET_DIR="/var/www/venus/gui-v2"
+BACKUP_DIR="/var/www/venus/gui-v2-backup"
+
+if [ ! -L "$BACKUP_DIR" ]; then
+    mv "$TARGET_DIR" "$BACKUP_DIR"
+fi
 
 wget -O /tmp/venus-webassembly.zip "$ZIP_URL"
 
-mkdir -p "$TARGET_DIR"
-
 unzip -o /tmp/venus-webassembly.zip "wasm/*" -d /tmp
-rm -R -f /var/www/venus/gui-v2-mod
+rm -R -f /var/www/venus/gui-v2
 
 mv /tmp/wasm "$TARGET_DIR"
-
-if [ ! -L "$BACKUP_SYMLINK" ]; then
-    cp -P "$SYMLINK" "$BACKUP_SYMLINK"
-fi
-
-ln -sfn "$TARGET_DIR" "$SYMLINK"
 
 rm /tmp/venus-webassembly.zip
